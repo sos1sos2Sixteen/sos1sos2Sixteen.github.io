61c9d7d04b97521f004e304eec802104081770c4
shiyao <sos1sos2harry@163.com>
2020-09-20 00:45:43 +0800

    basic construction

---
+++ datafiller.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+import os
+
+
+
+def main() : 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('webpage', type=str)
+    parser.add_argument('contents', type=str)
+    args = parser.parse_args()
+
+    with open(args.webpage) as f : 
+        webpage = f.read()
+
+    with open(args.contents) as f : 
+        contents = json.loads(f.read())
+
+    try : 
+        mapped_page = webpage.format(**contents)
+    except BaseException as e:
+        print(f'abort with : {e}')
+        exit(0)
+    
+    with open(os.path.join(os.path.dirname(args.webpage), f'c-{os.path.basename(args.webpage)}'), 'w') as f : 
+        f.write(mapped_page)
+
+
+if __name__ == '__main__' : 
+    main()
+
