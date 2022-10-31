cb6ca3c605d3401e3ab2bf7a1a381199a5aa85f7
shiyao <sos1sos2harry@163.com>
2022-10-31 21:42:18 +0800

    version 1 for voicelens

--- voicelens/script.js
+++ voicelens/script.js
@@ -1,6 +1,15 @@
-function clk_img(){
+function img_click(slf){
     console.log('clicked')
+    console.log(slf)
     let player = document.getElementById('global-player');
     player.src = "audios/raw/raw1.wav";
     player.play();
+}
+
+
+function play_audio(wavepath){
+    console.log(wavepath);
+    let player = document.getElementById('global-player');
+    player.src = wavepath
+    player.play()
 }
